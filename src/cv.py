a8e522009ab4e5c017c3c2f1f2483cef61a53f1a
Dylan Randle <dylanrandle@rogers.com>
2025-03-27 20:20:18 -0400

    chore: run formatter

--- src/cv.py
+++ src/cv.py
@@ -1,16 +1,20 @@
 import cv2
 import numpy as np
 
+
 def load_image(image_path):
     image = cv2.imread(image_path)
     return image
 
+
 def rgb_to_hsv(image):
     return cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+
 
 def wait():
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
 
 def mask_by_hsv(rgb_img, hsv_img, lower_hsv, upper_hsv, mask_color=(0, 0, 0)):
     lower_bound = np.array(lower_hsv)
@@ -22,6 +26,7 @@ def mask_by_hsv(rgb_img, hsv_img, lower_hsv, upper_hsv, mask_color=(0, 0, 0)):
     masked_img[mask > 0] = mask_color
 
     return masked_img
+
 
 green_min = [67, 159, 119]
 green_max = [88, 196, 243]
@@ -41,9 +46,8 @@ if __name__ == "__main__":
             print(f"HSV at ({x}, {y}): {hsv_color}")
             values.append(hsv_color)
 
-
-    cv2.imshow('Image', rgb)
-    cv2.setMouseCallback('Image', get_pixel_value)
+    cv2.imshow("Image", rgb)
+    cv2.setMouseCallback("Image", get_pixel_value)
 
     wait()
 
@@ -54,7 +58,7 @@ if __name__ == "__main__":
     print(f"min={values_min}, max={values_max}")
 
     mask = mask_by_hsv(rgb, hsv, values_min, values_max)
-    cv2.imshow('Original', rgb)
-    cv2.imshow('Mask', mask)
+    cv2.imshow("Original", rgb)
+    cv2.imshow("Mask", mask)
 
     wait()
