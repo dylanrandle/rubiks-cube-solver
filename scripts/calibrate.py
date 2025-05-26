ca2d0156564b1ac16897107b6272f2de55d099f1
Dylan Randle <dylanrandle@rogers.com>
2025-05-26 19:58:55 -0400

    add calibration scripts

---
+++ scripts/calibrate.py
@@ -0,0 +1,292 @@
+import argparse
+import logging
+from typing import Dict, List
+import json
+from pathlib import Path
+
+import cv2
+import numpy as np
+
+from rubiks_cube_solver.perception import (
+    Color,
+    Coordinate,
+    Face,
+    Image,
+    PerceptionSystem,
+    Position,
+)
+from rubiks_cube_solver.serial import ArduinoSerial
+
+
+CALIBRATION_PATH = Path(__file__).parent.parent / "data" / "calibration.json"
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--port", required=True, type=int, help="Port to arduino")
+    parser.add_argument(
+        "-m",
+        "--mode",
+        required=True,
+        choices={"colors", "coordinates"},
+        help="Calibration modality",
+    )
+    args = parser.parse_args()
+
+    serial = ArduinoSerial(args.port)
+    serial.wait_for_ready()
+
+    perception = PerceptionSystem(serial=serial)
+
+    images: Dict[Position, Image] = {}
+    for pos in Position:
+        img = perception.capture_image(pos)
+        images[pos] = img
+
+    if args.mode == "colors":
+        calibrate_colors(images)
+    elif args.mode == "coordinates":
+        calibrate_coordinates(images)
+    else:
+        raise RuntimeError(f"Unrecognized mode: {args.mode}")
+
+
+def calibrate_colors(images: Dict[Position, Image]):
+    for color in Color:
+        pixels_hsv = []
+        logging.info(f"Calibrating {color}")
+
+        def mouse_callback(img: Image):
+            def get_pixel_value(event, x, y, flags, param):
+                if event == cv2.EVENT_LBUTTONDOWN:
+                    hsv_color = img.hsv[y, x]
+                    logging.debug(f"HSV at ({x}, {y}): {hsv_color}")
+                    pixels_hsv.append(hsv_color)
+
+            return get_pixel_value
+
+        for pos in Position:
+            img = images[pos]
+            window_id = f"Camera: {pos}"
+            show_image(window_id, img.rgb, callback=mouse_callback(img))
+
+        wait(destroy=False)
+
+        if len(pixels_hsv) == 0:
+            logging.warning("Did not receive any selected pixels, skipping")
+            continue
+
+        hsv_min = np.min(pixels_hsv, axis=0)
+        hsv_max = np.max(pixels_hsv, axis=0)
+
+        logging.info(f"Color: {color} | min: {hsv_min} | max: {hsv_max}")
+
+        for pos in Position:
+            img = images[pos]
+            mask = mask_by_hsv(img, hsv_min, hsv_max)
+            window_id = f"Masked: {pos}"
+            show_image(window_id, mask)
+
+        wait(destroy=True)
+
+        logging.info("Commit? (y/n)")
+        response = input().strip().lower()
+        if response == "y":
+            save_color(color, hsv_min, hsv_max)
+        else:
+            logging.warning("Not saving results")
+
+
+def calibrate_coordinates(images: Dict[Position, Image]):
+    logging.info("""
+    Need to follow conventions used by solver:
+                |************|
+                |*U1**U2**U3*|
+                |************|
+                |*U4**U5**U6*|
+                |************|
+                |*U7**U8**U9*|
+                |************|
+    ************|************|************|************
+    *L1**L2**L3*|*F1**F2**F3*|*R1**R2**R3*|*B1**B2**B3*
+    ************|************|************|************
+    *L4**L5**L6*|*F4**F5**F6*|*R4**R5**R6*|*B4**B5**B6*
+    ************|************|************|************
+    *L7**L8**L9*|*F7**F8**F9*|*R7**R8**R9*|*B7**B8**B9*
+    ************|************|************|************
+                |************|
+                |*D1**D2**D3*|
+                |************|
+                |*D4**D5**D6*|
+                |************|
+                |*D7**D8**D9*|
+                |************|
+    """)
+    for face in Face:
+        logging.info(f"Calibrating face: {face}")
+        coordinates: List[Coordinate] = []
+
+        def get_coordinate(event, x, y, flags, param):
+            if event == cv2.EVENT_LBUTTONDOWN:
+                logging.debug(f"Coordinate: ({x}, {y})")
+                coordinates.append(Coordinate(x=x, y=y))
+
+        pos = Position(input("Select position (U, L): ").strip().upper())
+        img = images[pos]
+
+        window_id = f"Camera: {pos}"
+        show_image(window_id, img.rgb, callback=get_coordinate)
+
+        wait(destroy=False)
+
+        logging.info(f"Coordinates: {coordinates}")
+
+        annotated = img.rgb.copy()
+
+        for i, coord in enumerate(coordinates):
+            start = (coord.x - 5, coord.y - 5)
+            end = (coord.x + 5, coord.y + 5)
+
+            color = (0, 0, 0)
+            thickness = -1
+
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            font_scale = 1
+
+            annotated = cv2.rectangle(annotated, start, end, color, thickness)
+            annotated = cv2.putText(
+                annotated,
+                f"{i}",
+                (coord.x + 5, coord.y - 5),
+                font,
+                font_scale,
+                color,
+                5,
+            )
+
+        show_image(f"Annotated: {pos}", annotated)
+
+        wait(destroy=True)
+
+        logging.info("Commit? (y/n)")
+        response = input().strip().lower()
+        if response == "y":
+            save_coordinates(face, coordinates)
+
+
+def save_color(color: Color, hsv_min: np.ndarray, hsv_max: np.ndarray):
+    with open(CALIBRATION_PATH, "r") as f:
+        data = json.load(f)
+
+    data["colors"][color.value] = {
+        "min": hsv_min.tolist(),
+        "max": hsv_max.tolist(),
+    }
+
+    with open(CALIBRATION_PATH, "w") as f:
+        json.dump(data, f)
+
+
+def save_coordinates(face: Face, coordinates: List[Coordinate]):
+    with open(CALIBRATION_PATH, "r") as f:
+        data = json.load(f)
+
+    data["coordinates"][face.value] = [{"x": c.x, "y": c.y} for c in coordinates]
+
+    with open(CALIBRATION_PATH, "w") as f:
+        json.dump(data, f)
+
+
+def mask_by_hsv(img: Image, lower_hsv, upper_hsv, mask_color=(0, 0, 0)):
+    lower_bound = np.array(lower_hsv)
+    upper_bound = np.array(upper_hsv)
+
+    mask = cv2.inRange(img.hsv, lower_bound, upper_bound)
+
+    masked_img = img.rgb.copy()
+    masked_img[mask > 0] = mask_color
+
+    return masked_img
+
+
+def wait(destroy=True):
+    logging.info("Press `q` in the window to continue")
+    cv2.waitKey(0)
+    if destroy:
+        cv2.destroyAllWindows()
+
+
+def show_image(window_id: str, rgb: np.ndarray, callback=None):
+    cv2.namedWindow(window_id, cv2.WINDOW_NORMAL)
+    cv2.resizeWindow(window_id, rgb.shape[1] // 2, rgb.shape[0] // 2)
+    cv2.imshow(window_id, rgb)
+    if callback:
+        cv2.setMouseCallback(window_id, callback)
+
+
+if __name__ == "__main__":
+    main()
+
+
+# FACET_COORDINATES: Dict[Face, Iterable[Coordinate]] = {
+#     Face.BACK: [
+#         Coordinate(1040, 145),
+#         Coordinate(1200, 270),
+#         Coordinate(1379, 469),
+#         Coordinate(1031, 307),
+#         Coordinate(1366, 562),
+#         Coordinate(1010, 498),
+#         Coordinate(1224, 642),
+#         Coordinate(1395, 754),
+#     ],
+#     Face.LEFT: [
+#         Coordinate(533, 349),
+#         Coordinate(686, 427),
+#         Coordinate(869, 584),
+#         Coordinate(564, 550),
+#         Coordinate(903, 792),
+#         Coordinate(567, 642),
+#         Coordinate(730, 833),
+#         Coordinate(889, 969),
+#     ],
+#     Face.RIGHT: [
+#         Coordinate(524, 462),
+#         Coordinate(699, 264),
+#         Coordinate(860, 135),
+#         Coordinate(525, 557),
+#         Coordinate(857, 309),
+#         Coordinate(490, 755),
+#         Coordinate(644, 671),
+#         Coordinate(828, 550),
+#     ],
+#     Face.FRONT: [
+#         Coordinate(1089, 586),
+#         Coordinate(1274, 440),
+#         Coordinate(1450, 327),
+#         Coordinate(1089, 792),
+#         Coordinate(1418, 538),
+#         Coordinate(1065, 972),
+#         Coordinate(1237, 850),
+#         Coordinate(1425, 646),
+#     ],
+#     Face.UP: [
+#         Coordinate(620, 190),
+#         Coordinate(803, 111),
+#         Coordinate(1092, 46),
+#         Coordinate(797, 273),
+#         Coordinate(1175, 116),
+#         Coordinate(989, 403),
+#         Coordinate(1177, 284),
+#         Coordinate(1359, 189),
+#     ],
+#     Face.DOWN: [
+#         Coordinate(1046, 1058),
+#         Coordinate(766, 993),
+#         Coordinate(569, 912),
+#         Coordinate(1134, 991),
+#         Coordinate(750, 825),
+#         Coordinate(1322, 901),
+#         Coordinate(1149, 822),
+#         Coordinate(935, 721),
+#     ],
+# }
--- src/rubiks_cube_solver/perception.py
+++ src/rubiks_cube_solver/perception.py
@@ -3,6 +3,8 @@ import time
 from dataclasses import dataclass
 from enum import Enum
 from typing import Dict, Iterable
+import json
+from pathlib import Path
 
 import cv2
 import numpy as np
@@ -59,15 +61,14 @@ class Image:
 class PerceptionSystem:
     def __init__(self, serial: ArduinoSerial):
         self.serial = serial
+        self.calibration = load_calibration()
 
         self.position_to_camera_idx: Dict[Position, int] = {
-            Position.LOWER: 1,
-            Position.UPPER: 2,
+            Position.LOWER: 0,
+            Position.UPPER: 1,
         }
 
         self.light_prefix = "LIGHT:"
-        self.facet_coordinates = FACET_COORDINATES
-        self.hsv_ranges = HSV_RANGES
         self.position_to_faces: Dict[Position, Iterable[Face]] = {
             Position.UPPER: [Face.UP, Face.LEFT, Face.FRONT],
             Position.LOWER: [Face.RIGHT, Face.BACK, Face.DOWN],
@@ -94,12 +95,16 @@ class PerceptionSystem:
 
     def capture_image(self, position: Position):
         self.turn_light_on(position)
-        rgb = get_rgb(self.position_to_camera_idx[position])
-        self.turn_light_off(position)
+        try:
+            rgb = get_rgb(self.position_to_camera_idx[position])
+        except Exception as e:
+            raise e
+        finally:
+            self.turn_light_off(position)
         return Image(rgb=rgb, hsv=rgb_to_hsv(rgb))
 
     def get_face_colors(self, face: Face, image: Image):
-        coordinates = self.facet_coordinates[face]
+        coordinates = self.calibration.facet_coordinates[face]
         colors: Iterable[Color] = []
         for coordinate in coordinates:
             pixel_hsv = image.hsv[
@@ -108,10 +113,33 @@ class PerceptionSystem:
                 coordinate.x - self.color_neighborhood : coordinate.x
                 + self.color_neighborhood,
             ].mean(axis=(0, 1))
-            color = pixel_hsv_to_color(pixel_hsv)
+            color = self.pixel_hsv_to_color(pixel_hsv)
             colors.append(color)
             logging.debug(f"{face=}, {coordinate=}, {pixel_hsv=}, {color=}")
         return colors
+
+    def pixel_hsv_to_color(self, pixel_hsv: np.ndarray):
+        for color, range in self.calibration.hsv_ranges.items():
+            if np.all((range.min <= pixel_hsv) & (pixel_hsv <= range.max)):
+                return color
+
+        logging.warning(
+            f"Unable to find exact color match: {pixel_hsv=}, falling back to nearest"
+        )
+
+        closest_color = None
+        closest_distance = np.inf
+        for color, range in self.calibration.hsv_ranges.items():
+            distance = np.min(
+                [
+                    np.sum(np.abs(pixel_hsv - range.min)),
+                    np.sum(np.abs(pixel_hsv - range.max)),
+                ]
+            )
+            if distance < closest_distance:
+                closest_color = color
+                closest_distance = distance
+        return closest_color
 
     def get_cube_colors(self):
         cube_colors: Dict[Face, Iterable[Color]] = {}
@@ -165,103 +193,32 @@ def rgb_to_hsv(image):
     return cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
 
 
-def pixel_hsv_to_color(pixel_hsv: np.ndarray):
-    for color, range in HSV_RANGES.items():
-        if np.all((range.min <= pixel_hsv) & (pixel_hsv <= range.max)):
-            return color
-    raise RuntimeError(f"Unable to find a color match for {pixel_hsv=}")
+@dataclass
+class Calibration:
+    hsv_ranges: Dict[Color, Range]
+    facet_coordinates: Dict[Face, Iterable[Coordinate]]
 
 
-# Need to follow conventions used by solver:
-#             |************|
-#             |*U1**U2**U3*|
-#             |************|
-#             |*U4**U5**U6*|
-#             |************|
-#             |*U7**U8**U9*|
-#             |************|
-# ************|************|************|************
-# *L1**L2**L3*|*F1**F2**F3*|*R1**R2**R3*|*B1**B2**B3*
-# ************|************|************|************
-# *L4**L5**L6*|*F4**F5**F6*|*R4**R5**R6*|*B4**B5**B6*
-# ************|************|************|************
-# *L7**L8**L9*|*F7**F8**F9*|*R7**R8**R9*|*B7**B8**B9*
-# ************|************|************|************
-#             |************|
-#             |*D1**D2**D3*|
-#             |************|
-#             |*D4**D5**D6*|
-#             |************|
-#             |*D7**D8**D9*|
-#             |************|
-FACET_COORDINATES: Dict[Face, Iterable[Coordinate]] = {
-    Face.BACK: [
-        Coordinate(1040, 145),
-        Coordinate(1200, 270),
-        Coordinate(1379, 469),
-        Coordinate(1031, 307),
-        Coordinate(1366, 562),
-        Coordinate(1010, 498),
-        Coordinate(1224, 642),
-        Coordinate(1395, 754),
-    ],
-    Face.LEFT: [
-        Coordinate(533, 349),
-        Coordinate(686, 427),
-        Coordinate(869, 584),
-        Coordinate(564, 550),
-        Coordinate(903, 792),
-        Coordinate(567, 642),
-        Coordinate(730, 833),
-        Coordinate(889, 969),
-    ],
-    Face.RIGHT: [
-        Coordinate(524, 462),
-        Coordinate(699, 264),
-        Coordinate(860, 135),
-        Coordinate(525, 557),
-        Coordinate(857, 309),
-        Coordinate(490, 755),
-        Coordinate(644, 671),
-        Coordinate(828, 550),
-    ],
-    Face.FRONT: [
-        Coordinate(1089, 586),
-        Coordinate(1274, 440),
-        Coordinate(1450, 327),
-        Coordinate(1089, 792),
-        Coordinate(1418, 538),
-        Coordinate(1065, 972),
-        Coordinate(1237, 850),
-        Coordinate(1425, 646),
-    ],
-    Face.UP: [
-        Coordinate(620, 190),
-        Coordinate(803, 111),
-        Coordinate(1092, 46),
-        Coordinate(797, 273),
-        Coordinate(1175, 116),
-        Coordinate(989, 403),
-        Coordinate(1177, 284),
-        Coordinate(1359, 189),
-    ],
-    Face.DOWN: [
-        Coordinate(1046, 1058),
-        Coordinate(766, 993),
-        Coordinate(569, 912),
-        Coordinate(1134, 991),
-        Coordinate(750, 825),
-        Coordinate(1322, 901),
-        Coordinate(1149, 822),
-        Coordinate(935, 721),
-    ],
-}
+CALIBRATION_PATH = Path(__file__).parent.parent.parent / "data" / "calibration.json"
 
-HSV_RANGES: Dict[Color, Range] = {
-    Color.GREEN: Range(min=np.array([52, 96, 207]), max=np.array([57, 127, 239])),
-    Color.RED: Range(min=np.array([123, 176, 198]), max=np.array([128, 207, 223])),
-    Color.WHITE: Range(min=np.array([0, 0, 225]), max=np.array([179, 45, 255])),
-    Color.BLUE: Range(min=np.array([17, 114, 225]), max=np.array([19, 164, 255])),
-    Color.YELLOW: Range(min=np.array([78, 96, 199]), max=np.array([84, 118, 252])),
-    Color.ORANGE: Range(min=np.array([107, 146, 235]), max=np.array([111, 189, 255])),
-}
+
+def load_calibration() -> Calibration:
+    with open(CALIBRATION_PATH, "r") as f:
+        data: Dict[str, Dict] = json.load(f)
+
+    hsv_ranges: Dict[Color, Range] = {}
+
+    for color_id, color_range in data["colors"].items():
+        hsv_ranges[Color(color_id)] = Range(
+            min=np.array(color_range["min"]), max=np.array(color_range["max"])
+        )
+
+    facet_coordinates: Dict[Face, Iterable[Coordinate]] = {}
+
+    for face_id, face_coordinates in data["coordinates"].items():
+        facet_coordinates[Face(face_id)] = [
+            Coordinate(coordinate["x"], coordinate["y"])
+            for coordinate in face_coordinates
+        ]
+
+    return Calibration(hsv_ranges=hsv_ranges, facet_coordinates=facet_coordinates)
